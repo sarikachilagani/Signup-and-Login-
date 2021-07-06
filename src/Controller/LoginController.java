2bc5602fa7af2566fdc977a3e4abe8893ab5877b
sarikachilagani <84078414+sarikachilagani@users.noreply.github.com>
2021-07-06 11:20:42 +0530

    Update LoginController.java
    Formatted the code

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -9,48 +9,49 @@ public class LoginController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Scanner sc=new Scanner(System.in);
+
+		Scanner sc = new Scanner(System.in);
 		System.out.println("welcome!!! please enter Login or Signup:");
-		String value=sc.next();
-		switch(value.toLowerCase()) {
-		
-		case "login":{
+		String value = sc.next();
+
+		switch (value.toLowerCase()) {
+		case "login": {
 			System.out.println("To Login enter your username and password:");
-			String userName=sc.next();
-			String password=sc.next();
-			
-			LoginService ls=new LoginService();
-			boolean result=ls.save(userName, password);
-			if(result) {
+			String userName = sc.next();
+			String password = sc.next();
+
+			LoginService ls = new LoginService();
+			boolean result = ls.save(userName, password);
+			if (result) {
 				System.out.println("Login is successful");
-			}else {
+			} else {
 				System.out.println("Login failed");
 			}
-			
+
 			break;
 		}
-		case "signup":{
+		case "signup": {
 			System.out.println("Enter your username,password, email and phonenumber:");
-			String userName=sc.next();
-			String password=sc.next();
-			String email=sc.next();
-			int phoneNumber=sc.nextInt();
-			
-			SignupService ss=new SignupService();
-			boolean result =ss.save(userName,password,email,phoneNumber);
-			if(result) {
+			String userName = sc.next();
+			String password = sc.next();
+			String email = sc.next();
+			int phoneNumber = sc.nextInt();
+
+			SignupService ss = new SignupService();
+			boolean result = ss.save(userName, password, email, phoneNumber);
+			if (result) {
 				System.out.println("signup is successful");
-			}else {
+			} else {
 				System.out.println("signup failed");
 			}
 			break;
 		}
-		default:{
+		default: {
 			System.out.println("please enter valid details");
 		}
-		
+
 		}
 	}
 
 }
+
